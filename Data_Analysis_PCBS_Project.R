3af1fa1ea8f7d3cdcf530856db7886fa192f8772
KiraVanVoorhees <kira.vanvoorhees@gmail.com>
2021-05-12 12:35:46 +0200

    new

---
+++ Data_Analysis_PCBS_Project.R
@@ -0,0 +1,47 @@
+##Data analyses with no pre-processing in Python
+library(tidyverse)
+read.expyriment.data = function(folder, filename_pattern) 
+{
+  pattern = paste("^", filename_pattern, ".*\\.xpd", sep="") 
+  
+  data = data.frame()
+  for (fl_name in list.files(path=folder, pattern )) {
+    path = file.path(folder, fl_name)
+    message("reading ", path)
+    d = read.csv(path, comment.char="#", na.strings=c("NA", "None"))
+    fl = file(path, "r")
+    while(TRUE){
+      line = readLines(fl, n=1)
+      if (!length(line) || !length(grep("#", line)) )
+        break
+      else {
+        if (length(grep("^#s ", line))>0) {
+          tmp = unlist(strsplit(sub("#s ","", line), ":"))
+          if (length(tmp)<2) {
+            tmp = unlist(strsplit(sub("#s ","", line), "="))
+          }
+          if (grep("^ ", tmp[2]))
+            tmp[2] = substring(tmp[2], 2)
+          if (tmp[1] != "id") {
+            d = cbind(d, new = tmp[2])
+            names(d)[ncol(d)] = tmp[1]
+          }
+        }
+      }
+    }
+    close(fl)
+    if (nrow(data)<1) 
+      data = d
+    else 
+      data = rbind(data, d)
+  }
+  data
+}
+
+
+mydata <- read.expyriment.data("/Users/kiravanvoorhees/Desktop/PCBS/PCBS_Project/data", "distanceeffect")
+view(mydata)
+
+
+
+
