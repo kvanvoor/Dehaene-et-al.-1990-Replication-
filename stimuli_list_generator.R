e6012e14d8038a5d5e818cb55fdbefde30aa8dc7
KiraVanVoorhees <kira.vanvoorhees@gmail.com>
2021-05-12 21:36:54 +0200

    new

--- stimuli_list_generator.R
+++ stimuli_list_generator.R
@@ -1,8 +1,8 @@
-
+#generating list of all the stimuli
 
 list <- c(rep(41:54,4), rep(56:69,4),rep(11:40,2), rep(70:100,2))
 list1 <- sample(list)
 
 df1 <- as.data.frame(list1)
 
-write.csv(df, "df.csv")
+write.csv(df1, "df1.csv")
